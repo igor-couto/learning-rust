755c549d0fc984ce5ae9e3d13647d6d1ba153c64
Igor Couto <igor.fcouto@gmail.com>
2021-08-19 00:48:57 -0300

    First assignment, README and gitignore

---
+++ assignment_01/target/debug/deps/assignment_01-97eadf7a91c42eb6.d
@@ -0,0 +1,5 @@
+c:\code\igor-couto\learning-rust\assignment_01\target\debug\deps\assignment_01-97eadf7a91c42eb6.rmeta: src\main.rs
+
+c:\code\igor-couto\learning-rust\assignment_01\target\debug\deps\assignment_01-97eadf7a91c42eb6.d: src\main.rs
+
+src\main.rs:
---
+++ assignment_01/target/debug/deps/assignment_01-9bb05bb4d2c58d65.d
@@ -0,0 +1,5 @@
+c:\code\igor-couto\learning-rust\assignment_01\target\debug\deps\assignment_01-9bb05bb4d2c58d65.rmeta: src\main.rs
+
+c:\code\igor-couto\learning-rust\assignment_01\target\debug\deps\assignment_01-9bb05bb4d2c58d65.d: src\main.rs
+
+src\main.rs:
