d63d9b327462eb11881491f2a5504907301169d2
Igor Couto <igor.fcouto@gmail.com>
2021-08-19 02:17:21 -0300

    Assignment 02 fix

--- assignment_02/src/main.rs
+++ assignment_02/src/main.rs
@@ -1,26 +1,18 @@
 // Construir um algoritmo para ler 5 valores inteiros, calcular e imprimir a soma desses valores.
 
+use::std::env;
+
 fn main() {
-    
-    let n1 = 1;
-    let n2 = 2;
-    let n3 = 3;
-    let n4 = 4;
-    let n5 = 5;
+
+    let args: Vec<String> = env::args().collect();
+
+    let n1 = args[1].parse::<i32>().unwrap();
+    let n2 = args[2].parse::<i32>().unwrap();
+    let n3 = args[3].parse::<i32>().unwrap();
+    let n4 = args[4].parse::<i32>().unwrap();
+    let n5 = args[5].parse::<i32>().unwrap();
     
     let sum = n1 + n2 + n3 + n4 + n5;
     
-    println!("O resultado é {}", vectorApproach());
-}
-
-fn vectorApproach() -> i32
-{
-    let numbers = [1, 2, 3, 4, 5];
-    let mut sum = 0;
-    for number in numbers
-    {
-        sum += number;
-    } 
-
-    sum
+    println!("O resultado é {}", sum);
 }
