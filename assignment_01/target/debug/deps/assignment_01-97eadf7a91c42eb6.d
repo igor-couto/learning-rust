0136eeca7e9df09a5efb7c2d474ddf39be8d24b7
Igor Couto <igor.fcouto@gmail.com>
2021-08-21 19:58:18 -0300

    Delete assignment_01/target directory

--- assignment_01/target/debug/deps/assignment_01-97eadf7a91c42eb6.d
+++
@@ -1,5 +0,0 @@
-c:\code\igor-couto\learning-rust\assignment_01\target\debug\deps\assignment_01-97eadf7a91c42eb6.rmeta: src\main.rs
-
-c:\code\igor-couto\learning-rust\assignment_01\target\debug\deps\assignment_01-97eadf7a91c42eb6.d: src\main.rs
-
-src\main.rs:
--- assignment_01/target/debug/deps/assignment_01-9bb05bb4d2c58d65.d
+++
@@ -1,5 +0,0 @@
-c:\code\igor-couto\learning-rust\assignment_01\target\debug\deps\assignment_01-9bb05bb4d2c58d65.rmeta: src\main.rs
-
-c:\code\igor-couto\learning-rust\assignment_01\target\debug\deps\assignment_01-9bb05bb4d2c58d65.d: src\main.rs
-
-src\main.rs:
